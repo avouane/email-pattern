feb8a73c124ce100503248808968d36b0055493b
avouane <avouane7@gmail.com>
2026-09-10 11:30:06 +0500

    add skeleton

--- src/builder/email/EmailPreviewBuilder.java
+++ src/builder/email/EmailPreviewBuilder.java
@@ -1,4 +1,9 @@
 package builder.email;
 
-public class EmailPreviewBuilder {
+public class EmailPreviewBuilder implements EmailBuilder {
+    private final StringBuilder sb = new StringBuilder("=== Email Preview ===\n");
+
+    public EmailPreviewBuilder setFrom(String from) { sb.append("From:    ").append(from).append("\n"); return this; }
+    public EmailPreviewBuilder setTo(String to) { sb.append("To:      ").append(to).append("\n"); return this; }
+
 }
