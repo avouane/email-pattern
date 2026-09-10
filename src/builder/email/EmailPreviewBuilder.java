654eab20c1831439ed9dfa71a9f67ddc4727e914
avouane <avouane7@gmail.com>
2026-09-10 11:31:52 +0500

    add setters

--- src/builder/email/EmailPreviewBuilder.java
+++ src/builder/email/EmailPreviewBuilder.java
@@ -2,8 +2,12 @@ package builder.email;
 
 public class EmailPreviewBuilder implements EmailBuilder {
     private final StringBuilder sb = new StringBuilder("=== Email Preview ===\n");
-
-    public EmailPreviewBuilder setFrom(String from) { sb.append("From:    ").append(from).append("\n"); return this; }
-    public EmailPreviewBuilder setTo(String to) { sb.append("To:      ").append(to).append("\n"); return this; }
-
+    public EmailPreviewBuilder setFrom(String from) {
+        sb.append("From:    ").append(from).append("\n"); return this; }
+    public EmailPreviewBuilder setTo(String to) {
+        sb.append("To:      ").append(to).append("\n"); return this; }
+    public EmailPreviewBuilder setSubject(String subject) {
+        sb.append("Subject: ").append(subject).append("\n"); return this; }
+    public EmailPreviewBuilder setBody(String body) {
+        sb.append("Body:    ").append(body).append("\n"); return this; }
 }
