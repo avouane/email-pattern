0f370ac2bf7713f7a96fea8c658c33154a90299b
avouane <avouane7@gmail.com>
2026-09-10 11:25:45 +0500

    add EmailObjectBuilder skeleton with fluent setters

--- src/builder/email/EmailObjectBuilder.java
+++ src/builder/email/EmailObjectBuilder.java
@@ -1,4 +1,10 @@
 package builder.email;
 
-public class EmailObjectBuilder {
+public class EmailObjectBuilder implements EmailBuilder {
+    private String from, to, subject, body;
+
+    public EmailObjectBuilder setFrom(String from) { this.from = from; return this; }
+    public EmailObjectBuilder setTo(String to) { this.to = to; return this; }
+    public EmailObjectBuilder setSubject(String subject) { this.subject = subject; return this; }
+    public EmailObjectBuilder setBody(String body) { this.body = body; return this; }
 }
