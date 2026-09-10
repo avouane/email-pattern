4009ee9228f2e756a94b6abb154e778e570cc299
avouane <avouane7@gmail.com>
2026-09-10 11:51:56 +0500

    Extend Main to also demonstrate text preview representation

--- src/builder/Main.java
+++ src/builder/Main.java
@@ -1,4 +1,8 @@
-package builder.app;
+package builder;
+import builder.email.Email;
+import builder.email.EmailDirector;
+import builder.email.EmailObjectBuilder;
+import builder.email.EmailPreviewBuilder;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,5 +11,9 @@ public class Main {
         director.makeWelcomeEmail(objBuilder, "user@example.com");
         Email welcomeEmail = objBuilder.getResult();
         System.out.println(welcomeEmail);
+        EmailPreviewBuilder previewBuilder = new EmailPreviewBuilder();
+        director.makeWelcomeEmail(previewBuilder, "user@example.com");
+        String preview = previewBuilder.getResult();
+        System.out.println(preview);
     }
 }
